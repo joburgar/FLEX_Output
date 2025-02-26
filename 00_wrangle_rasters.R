db1c5bc9d69cf5b6bae816697476e01303a99131
Burgar <Joanna.Burgar@gov.bc.ca>
2025-02-26 11:34:18 -0800

    Creating code to stack rasters from FAIB, and add a pixelID layer. Also updating code to produce output (HR summaries, plotting, and raster) from FLEX runs.

---
+++ 00_wrangle_rasters.R
@@ -0,0 +1,132 @@
+#####################################################################################
+R_version <- paste0("R-",version$major,".",version$minor)
+.libPaths(paste0("C:/Program Files/R/",R_version,"/library")) # to ensure reading/writing libraries from C drive
+
+# install.packages("terra")
+library(terra)
+library(tidyverse)
+library(data.table)
+library(sf)
+library(bcdata)
+# Save raster with layer names
+
+# List all raster files in a folder
+r_files <- list.files(paste0(here::here(), "/Input_files/"),pattern = "\\.tif$", full.names = TRUE)
+
+# Load and stack them
+raster_stack <- rast(r_files)
+
+# Check the stack
+print(raster_stack)
+# plot(raster_stack)
+
+# Load the polygon shapefile (SpatVector)
+GIS_Dir <- "//sfp.idir.bcgov/S140/S40203/Ecosystems/Conservation Science/Species Conservation Science/Mesocarnivores/Projects/MMP/2.Data/Mesocarnivores DB/5. GIS"
+# list.files(GIS_Dir) # check it works
+subpopulations <- sf::st_read(dsn = file.path(GIS_Dir, "BC_Fisher_populations_2024.gdb"), layer = "Subpopulations")
+
+Boreal <- subpopulations |> dplyr::filter(Subpop == "Boreal")
+rm(subpopulations)# housekeeping
+# Find TSAs within Boreal
+
+# bcdc_search("tsa", res_format = "wms")
+# 1: FADM - Timber Supply Area (TSA) (multiple, wms, kml, oracle_sde)
+# ID: 8daa29da-d7f4-401c-83ae-d962e3a28980
+# Name: fadm-timber-supply-area-tsa
+tsa <- bcdc_get_data("8daa29da-d7f4-401c-83ae-d962e3a28980")
+
+# save.image("wrangle_rasters_inputs.RData")
+# load("wrangle_rasters_inputs.RData")
+
+Boreal_tsa <- tsa %>% sf::st_intersection(Boreal)
+ggplot()+
+  geom_sf(data = Boreal_tsa, aes(col=TSA_NUMBER))
+
+rm(tsa)# housekeeping
+
+Boreal_tsa %>% group_by(TSA_NUMBER) %>% summarise(sum(Area)) 
+
+
+Boreal_tsa_merged <- Boreal_tsa %>%
+  group_by(TSA_NUMBER) %>%
+  summarise(geometry = sf::st_union(geometry), .groups = "drop")
+
+Boreal_tsa_merged <- Boreal_tsa_merged %>% dplyr::mutate(area_meters = st_area(Boreal_tsa_merged))
+Boreal_tsa_merged %>% st_drop_geometry()
+
+ggplot()+
+  geom_sf(data = Boreal_tsa_merged, aes(col=TSA_NUMBER))
+
+Boreal_tsa_merged$TSA_NUMBER <- as.factor(Boreal_tsa_merged$TSA_NUMBER)
+Boreal_tsa %>% group_by(TSA_NUMBER) %>% count(TSA_NUMBER_DESCRIPTION) %>% sf::st_drop_geometry()
+# 04         Cassiar TSA                6
+# 08         Fort Nelson TSA            6
+# 16         MacKenzie TSA              5
+# 40         Fort St. John TSA          7
+# 41         Dawson Creek TSA           5
+rm(Boreal_tsa)
+
+# Ensure both have the same CRS
+crs(raster_stack) <- crs(Boreal_tsa_merged)  # Set CRS if needed
+
+FLEXraster <- function(tsa_merged) {
+  
+  # Filter TSA region
+  # aoi_filtered <- Boreal_tsa_merged %>% filter(TSA_NUMBER == "40")
+  aoi_filtered <- tsa_merged %>% filter(TSA_NUMBER == "41")
+  
+  # Clip and mask raster to AOI
+  clipped_raster <- crop(raster_stack, aoi_filtered)  # Crop to bounding box
+  clipped_raster <- mask(clipped_raster, aoi_filtered)  # Mask to exact shape
+
+  # Load an example raster (assuming clipped_raster is already loaded)
+  pixel_id <- clipped_raster[[1]]  # Use first layer as a template
+  
+  # Create a sequence of unique IDs for all pixels, including NA ones
+  pixel_id[] <- as.integer(seq_len(ncell(pixel_id)))  # Assign unique ID to every pixel
+  
+  # Convert to integer type
+  pixel_id <- round(pixel_id)
+  
+  # Add pixel ID layer to the raster stack
+  updated_stack <- c(clipped_raster, pixel_id)
+  
+  # Rename the new layer for clarity
+  names(updated_stack)[nlyr(updated_stack)] <- "Pixel_ID"
+  
+  # Plot to check the result
+  plot(updated_stack)
+  
+  # Check layer names
+  print(names(updated_stack))
+  
+  # Reorder layers
+  layer_names <- c("Pixel_ID", "fisher_pop", "denning2023", "rest_cavity2023", 
+                   "movement2023", "rest_rust2023", "rest_cwd2023", "open2023")
+  
+  if (!all(layer_names %in% names(updated_stack))) {
+    stop("One or more expected layers are missing in clipped_raster.")
+  }
+  
+  new_order <- updated_stack[[layer_names]]
+  
+  # Rename layers for FLEX
+  flex_names <- c("pixelid", "ras_fisher_pop", "ras_fisher_denning_init", "ras_fisher_cavity_init", 
+                  "ras_fisher_movement_init", "ras_fisher_rust_init", "ras_fisher_cwd_init", 
+                  "ras_fisher_open_init")
+  
+  names(new_order) <- flex_names
+  
+  # Plot final result
+  plot(new_order)
+  
+  return(new_order)
+}
+
+Boreal_DC <- FLEXraster(tsa_merged = Boreal_tsa_merged)
+
+# Save the  raster
+writeRaster(Boreal_DC, "Boreal_DC.tif", datatype = "INT4S",overwrite=TRUE) # for large rasters, need to provide data type
+
+
+
---
+++ 01_load_data.R
@@ -0,0 +1,100 @@
+# Copyright 2021 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+#
+#####################################################################################
+# 01_load.R
+# script to load FLEX output files
+# written by Joanna Burgar (Joanna.Burgar@gov.bc.ca) - 06-Nov-2024
+#####################################################################################
+R_version <- paste0("R-",version$major,".",version$minor)
+.libPaths(paste0("C:/Program Files/R/",R_version,"/library")) # to ensure reading/writing libraries from C drive
+
+# Load Packages
+list.of.packages <- c("tidyverse","sf", "raster","fasterize","Cairo")
+
+# Check you have them and load them
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only = TRUE)
+#####################################################################################
+
+FLEX_name <- "Boreal_Cassiar"
+FLEX_dir <- "./Output_files/"
+list.files(paste0(FLEX_dir, FLEX_name))
+list.files(getwd())
+
+FLEX_files <- list.files(paste0(FLEX_dir, FLEX_name))
+FLEX_final_fisher_territories <- FLEX_files[grep("*final_fisher_territories", FLEX_files, ignore.case = FALSE)]
+
+FLEX_list=list()
+hr_list = list()
+
+for(i in 1:length(FLEX_final_fisher_territories)){
+  # i=1
+  rFLEX <- raster(paste0(FLEX_dir, FLEX_name, "/", FLEX_final_fisher_territories[i]))
+
+  plot(rFLEX)
+  
+  # create hr table to summarise number and size of hr per output
+  hr <- as_tibble(freq(rFLEX))
+  hr <- hr %>% dplyr::filter(value>0)
+  hr$HRNum <- rownames(hr)
+  hr$Areakm2 <- hr$count * 0.01 #convert from ha to km2
+  hr$Name <- paste0(FLEX_name,i)
+  hr %>% summarise(min(Areakm2), mean (Areakm2), max(Areakm2))
+  
+  hr_list[[i]] <- hr
+  
+  rFLEX[rFLEX > 0] <- 1
+  FLEX_list[[i]] <- rFLEX 
+  
+  # return(list(hr_list, FLEX_list))
+
+}
+
+FLEX_hr <- data.table::rbindlist(hr_list)
+FLEX_hr_sum <- FLEX_hr %>% group_by(Name) %>% summarise(mean(Areakm2), sd(Areakm2), min(Areakm2), max(Areakm2))
+colnames(FLEX_hr_sum) <- c("FLEX_run","mean_Areakm2", "sd_Areakm2","min_Areakm2","max_Areakm2")
+numHR <- FLEX_hr %>% group_by(Name) %>% count(Name) %>% ungroup()  %>% dplyr::select(n)
+FLEX_hr_sum$countHR <- numHR$n
+
+FLEX_hr_sum %>% summarise(mean(countHR), min(countHR), max(countHR))
+FLEX_hr_sum %>% summarise(mean(mean_Areakm2), min(min_Areakm2), max(max_Areakm2))
+
+FLEX_stack = stack(FLEX_list)
+FLEX_sumstack <- stackApply(FLEX_stack, indices=1, fun=sum)
+plot(FLEX_sumstack)
+writeRaster(FLEX_sumstack, file=paste0(FLEX_dir, FLEX_name, "/", FLEX_name, "_stack.tif"), bylayer=TRUE)
+
+
+# Reclassify values using a custom function
+r_new <- calc(FLEX_sumstack, function(x) {
+  ifelse(x > 0 & x <= 50, 1,
+         ifelse(x > 50, 2, 0)
+  )
+})
+plot(r_new)
+values(r_new)
+writeRaster(r_new, file=paste0(FLEX_dir, FLEX_name, "/", FLEX_name, "_VRI2021_LH.tif"), bylayer=TRUE)
+
+
+
+# sum(values(FLEX_sumstack)) # 460323 ha predicted fisher habitat in ref scenario; 551820 in nharv_thlb; and 555728 in nharv scenario
+
+# to make binary habitat / no habitat raster layers
+FLEX_sumstack[values(FLEX_sumstack)>0] <- 1
+
+writeRaster(FLEX_sumstack, file=paste0(FLEX_dir, FLEX_name, "/", FLEX_name, "_VRI2021_binary.tif"), bylayer=TRUE)
+
+Cairo(file=paste0(FLEX_dir, FLEX_name, "/", FLEX_name, "_VRI2021_binary.png"), type="png", width=1800, height=2200,pointsize=15,bg="white",dpi=300)
+plot(FLEX_sumstack)
+dev.off()
