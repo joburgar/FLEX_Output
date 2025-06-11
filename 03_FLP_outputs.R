a856f40b04b5cf200016fc8a88943003c0eaf590
Burgar <Joanna.Burgar@gov.bc.ca>
2025-06-11 09:54:09 -0700

    Creating FLEX output for Nazko area

--- 03_FLP_outputs.R
+++ 03_FLP_outputs.R
@@ -23,20 +23,28 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only = TRUE)
 #####################################################################################
+# For Quesnel
+# Quesnel <- sf::st_read(dsn=here::here(), layer="Quesnel_BEC")
+# Quesnel  %>% group_by(ZONE) %>% summarise(sum(Area_sqkm))
+# ggplot()+
+#   geom_sf(data=Quesnel)
+# Quesnel %>% glimpse() %>% st_drop_geometry()
+# Quesnel_merged <- Quesnel %>%
+#   summarise(geometry = sf::st_union(geometry), .groups = "drop")
+# ggplot()+geom_sf(data=Quesnel_merged)
+# st_area(Quesnel_merged)*1e-6
+# aoi_filtered <- Quesnel_merged
 
-Quesnel <- sf::st_read(dsn=here::here(), layer="Quesnel_BEC")
-Quesnel  %>% group_by(ZONE) %>% summarise(sum(Area_sqkm))
+
+### Nazko area
+Nazko <- sf::st_read(dsn="//sfp.idir.bcgov/S140/S40203/Ecosystems/Conservation Science/Species/Mesocarnivores/Projects/FLM/2. Data/2.1 GIS",
+                     layer="clip_v3_merge")
 ggplot()+
-  geom_sf(data=Quesnel)
-Quesnel %>% glimpse() %>% st_drop_geometry()
-Quesnel_merged <- Quesnel %>%
-  summarise(geometry = sf::st_union(geometry), .groups = "drop")
-ggplot()+geom_sf(data=Quesnel_merged)
-st_area(Quesnel_merged)*1e-6
+  geom_sf(data=Nazko)
 
-aoi_filtered <- Quesnel_merged
+aoi_filtered <- Nazko
 
-FLEX_name <- "Cariboo"
+FLEX_name <- "Omineca"
 FLEX_dir <- "./Output_files/"
 list.files(paste0(FLEX_dir, FLEX_name))
 
@@ -84,56 +92,23 @@ FLEX_hr_sum %>% summarise(mean(countHR), min(countHR), max(countHR))
 FLEX_hr_sum %>% summarise(mean(mean_Areakm2), min(min_Areakm2), max(max_Areakm2))
 
 
-FLEX_name <- "Chilcotin"
-FLEX_dir <- "./Output_files/"
-list.files(paste0(FLEX_dir, FLEX_name))
+# clip each raster to the Nazko shapefile
+# Ensure both have the same CRS
 
-FLEX_files <- list.files(paste0(FLEX_dir, FLEX_name))
-FLEX_final_fisher_territories <- FLEX_files[grep("*final_fisher_territories", FLEX_files, ignore.case = FALSE)]
-# FLEX_final_fisher_territories <- FLEX_files[grep("_(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25)_.*final_fisher_territories", FLEX_files)]
+# Filter to fisher region of interest
+aoi_filtered <- Nazko
+FLEXraster <- rast(paste0(getwd(),"/Omineca5_stack.tif"))
 
-FLEX_list=list()
-hr_list = list()
+# Clip and mask raster to AOI
+clipped_raster <- crop(FLEXraster, aoi_filtered)  # Crop to bounding box
+clipped_raster <- mask(clipped_raster, aoi_filtered)  # Mask to exact shape
 
-
-for(i in 1:length(FLEX_final_fisher_territories)){
-  # i=1
-  rFLEX <- raster(paste0(FLEX_dir, FLEX_name, "/", FLEX_final_fisher_territories[i]))
-  clipped_rFLEX <- crop(rFLEX, aoi_filtered)  # Crop to bounding box
-  clipped_rFLEX <- mask(clipped_rFLEX, aoi_filtered)  # Mask to exact shape
-  
-  
-  plot(clipped_rFLEX)
-  
-  # create hr table to summarise number and size of hr per output
-  hr <- as_tibble(freq(clipped_rFLEX))
-  hr <- hr %>% dplyr::filter(value>0)
-  hr$HRNum <- rownames(hr)
-  hr$Areakm2 <- hr$count * 0.01 #convert from ha to km2
-  hr$Name <- paste0(FLEX_name,i)
-  hr %>% summarise(min(Areakm2), mean (Areakm2), max(Areakm2))
-  
-  hr_list[[i]] <- hr
-  
-  rFLEX[rFLEX > 0] <- 1
-  FLEX_list[[i]] <- rFLEX 
-  
-  # return(list(hr_list, FLEX_list))
-  
-}
-
-FLEX_hr <- data.table::rbindlist(hr_list)
-FLEX_hr_sum <- FLEX_hr %>% group_by(Name) %>% summarise(mean(Areakm2), sd(Areakm2), min(Areakm2), max(Areakm2))
-colnames(FLEX_hr_sum) <- c("FLEX_run","mean_Areakm2", "sd_Areakm2","min_Areakm2","max_Areakm2")
-numHR <- FLEX_hr %>% group_by(Name) %>% count(Name) %>% ungroup()  %>% dplyr::select(n)
-FLEX_hr_sum$countHR <- numHR$n
-
-FLEX_hr_sum %>% summarise(mean(countHR), min(countHR), max(countHR))
-FLEX_hr_sum %>% summarise(mean(mean_Areakm2), min(min_Areakm2), max(max_Areakm2))
+plot(clipped_raster)
 
 
-writeRaster(clipped_raster1, "Quesnel_Cariboo.tif", datatype = "INT4S",overwrite=TRUE) # for large rasters, need to provide data type
-writeRaster(clipped_raster2, "Quesnel_Chicotin.tif", datatype = "INT4S",overwrite=TRUE) # for large rasters, need to provide data type
+writeRaster(clipped_raster, "Nazko_Cariboo.tif", datatype = "INT4S",overwrite=TRUE) # for large rasters, need to provide data type
+writeRaster(clipped_raster, "Nazko_Chilcotin.tif", datatype = "INT4S",overwrite=TRUE) # for large rasters, need to provide data type
+writeRaster(clipped_raster, "Nazko_Omineca.tif", datatype = "INT4S",overwrite=TRUE) # for large rasters, need to provide data type
 
 #####################################################################################
 #####################################################################################
@@ -211,7 +186,7 @@ ggplot(data = Q_NoHarvest) +
 
 #########################################################
 # Ensure both have the same CRS
-Q_NoHarvest <- st_transform(Q_NoHarvest, crs = st_crs(Q_chilcotin_reclass))
+aoi <- st_transform(Q_NoHarvest, crs = st_crs(Q_chilcotin_reclass))
 
 # Original buffers (may overlap)
 buffers <- st_buffer(Q_NoHarvest, dist = 2000, nQuadSegs = 60)
@@ -245,8 +220,27 @@ ggplot() +
 
 
 ###################################################
+## Bring in rasters of interest
+# To refine priority areas within FLP
+# Either run through the above code or bring in layers already created
+
+# If needing to bring in raster files
+Q_cariboo <- rast(paste0(getwd(),"/Quesnel_Cariboo.tif"))
+Q_chilcotin <- rast(paste0(getwd(),"/Quesnel_Chicotin.tif"))
+
+# Check the rasters
+print(Q_cariboo)
+plot(Q_cariboo)
+
+print(Q_chilcotin)
+plot(Q_chilcotin)
+
+# Check values
+hist(values(Q_cariboo))
+hist(values(Q_chilcotin))
+
 # Filter to fisher region of interest
-aoi_filtered <- buffers_no_overlap_sf
+
 aoi_filtered <- st_transform(aoi_filtered, crs = st_crs(Q_cariboo_reclass))
 
 
