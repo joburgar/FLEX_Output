1bd247e569cd9f09a69cd50ce6bcfa384bbf415d
Burgar <Joanna.Burgar@gov.bc.ca>
2025-06-19 20:11:12 -0700

    tidying up code to make it run smoothly and more generically

--- 03_FLP_outputs.R
+++ 03_FLP_outputs.R
@@ -16,7 +16,7 @@
 # written by Joanna Burgar (Joanna.Burgar@gov.bc.ca) - 30-May-2025
 #####################################################################################
 # Load Packages
-list.of.packages <- c("terra","tidyverse","sf", "raster","Cairo","data.table")
+list.of.packages <- c("terra","tidyverse","sf", "Cairo", "data.table")
 
 # Check you have them and load them
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -35,15 +35,19 @@ lapply(list.of.packages, require, character.only = TRUE)
 # st_area(Quesnel_merged)*1e-6
 # aoi_filtered <- Quesnel_merged
 
-
 ### Area of Interest
-aoi <- sf::st_read(dsn="//sfp.idir.bcgov/S140/S40203/Ecosystems/Conservation Science/Species/Mesocarnivores/Projects/FLM/2. Data/2.1 GIS",
-                     layer="clip_v3_merge")
+DSN <- "//sfp.idir.bcgov/S140/S40203/Ecosystems/Conservation Science/Species/Mesocarnivores/Projects/Enterprise Fisher Telemetry/2. Data/Scat_Surveys"
+aoi_layer <- "MF_Diet_StudyArea"
+
+aoi <- sf::st_read(dsn=DSN, layer=aoi_layer)
+aoi <- st_zm(aoi)
+aoi_merged <- st_union(aoi)
+
 ggplot()+
-  geom_sf(data=aoi)
+  geom_sf(data=aoi_merged)
 
-
-FLEX_name <- "Omineca"
+# raster
+FLEX_name <- "chilcotin"
 FLEX_dir <- "./Output_files/"
 list.files(paste0(FLEX_dir, FLEX_name))
 
@@ -51,35 +55,52 @@ FLEX_files <- list.files(paste0(FLEX_dir, FLEX_name))
 FLEX_final_fisher_territories <- FLEX_files[grep("*final_fisher_territories", FLEX_files, ignore.case = FALSE)]
 # FLEX_final_fisher_territories <- FLEX_files[grep("_(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25)_.*final_fisher_territories", FLEX_files)]
 
-FLEX_list=list()
-hr_list = list()
+hr_list <- list()
+FLEX_list <- list()
 
-
-for(i in 1:length(FLEX_final_fisher_territories)){
-  # i=1
-  rFLEX <- raster(paste0(FLEX_dir, FLEX_name, "/", FLEX_final_fisher_territories[i]))
-  clipped_rFLEX <- crop(rFLEX, aoi)  # Crop to bounding box
-  clipped_rFLEX <- mask(clipped_rFLEX, aoi)  # Mask to exact shape
-
+for (i in seq_along(FLEX_final_fisher_territories)) {
+  # Read raster using terra
+  rFLEX <- rast(paste0(FLEX_dir, FLEX_name, "/", FLEX_final_fisher_territories[i]))
   
+  # Align AOI CRS to raster CRS
+  aoi_merged_aligned <- st_transform(aoi_merged, crs = crs(rFLEX))
+  
+  # Convert AOI to SpatVector
+  aoi_vect <- vect(aoi_merged_aligned)
+  
+  # Crop and mask
+  clipped_rFLEX <- crop(rFLEX, aoi_vect)
+  clipped_rFLEX <- mask(clipped_rFLEX, aoi_vect)
+  
+  # Plot for quick check
   plot(clipped_rFLEX)
   
-  # create hr table to summarise number and size of hr per output
+  # Frequency table
   hr <- as_tibble(freq(clipped_rFLEX))
-  hr <- hr %>% dplyr::filter(value>0)
-  hr$HRNum <- rownames(hr)
-  hr$Areakm2 <- hr$count * 0.01 #convert from ha to km2
-  hr$Name <- paste0(FLEX_name,i)
-  hr %>% summarise(min(Areakm2), mean (Areakm2), max(Areakm2))
+  hr <- hr %>% filter(value > 0)
   
+  # Calculate area per pixel in km²
+  pixel_area_km2 <- prod(res(clipped_rFLEX)) / 1e6
+  
+  # Add HR number, area, and name
+  hr$HRNum <- seq_len(nrow(hr))
+  hr$Areakm2 <- hr$count * pixel_area_km2
+  hr$Name <- paste0(FLEX_name, i)
+  
+  # Print summary stats
+  print(hr %>% summarise(min_Areakm2 = min(Areakm2),
+                         mean_Areakm2 = mean(Areakm2),
+                         max_Areakm2 = max(Areakm2)))
+  
+  # Save to lists
   hr_list[[i]] <- hr
   
-  rFLEX[rFLEX > 0] <- 1
-  FLEX_list[[i]] <- rFLEX 
-  
-  # return(list(hr_list, FLEX_list))
-  
+  # Binarize original rFLEX and save
+  rFLEX_bin <- rFLEX
+  rFLEX_bin[rFLEX_bin > 0] <- 1
+  FLEX_list[[i]] <- rFLEX_bin
 }
+
 
 FLEX_hr <- data.table::rbindlist(hr_list)
 FLEX_hr_sum <- FLEX_hr %>% group_by(Name) %>% summarise(mean(Areakm2), sd(Areakm2), min(Areakm2), max(Areakm2))
@@ -91,21 +112,18 @@ FLEX_hr_sum %>% summarise(mean(countHR), min(countHR), max(countHR))
 FLEX_hr_sum %>% summarise(mean(mean_Areakm2), min(min_Areakm2), max(max_Areakm2))
 
 
-# clip each raster to the aoi shapefile
-# Ensure both have the same CRS
-
+# To just get the clipped output (without # of predicted territories)
 # Filter to fisher region of interest
-FLEXraster <- rast(paste0(getwd(),"/Omineca5_stack.tif"))
+FLEXraster <- rast(paste0(getwd(),"/Chilcotin5_stack.tif"))
 
 # Clip and mask raster to AOI
-clipped_raster <- crop(FLEXraster, aoi)  # Crop to bounding box
-clipped_raster <- mask(clipped_raster, aoi)  # Mask to exact shape
+clipped_raster <- crop(FLEXraster, aoi_vect)  # Crop to bounding box
+clipped_raster <- mask(clipped_raster, aoi_vect)  # Mask to exact shape
 
 plot(clipped_raster)
 
-
-# writeRaster(clipped_raster, "Nazko_Cariboo.tif", datatype = "INT4S",overwrite=TRUE) # for large rasters, need to provide data type
-# writeRaster(clipped_raster, "Nazko_Chilcotin.tif", datatype = "INT4S",overwrite=TRUE) # for large rasters, need to provide data type
+writeRaster(clipped_raster, "MF_Cariboo.tif", datatype = "INT4S",overwrite=TRUE) # for large rasters, need to provide data type
+writeRaster(clipped_raster, "MF_Chilcotin.tif", datatype = "INT4S",overwrite=TRUE) # for large rasters, need to provide data type
 # writeRaster(clipped_raster, "Nazko_Omineca.tif", datatype = "INT4S",overwrite=TRUE) # for large rasters, need to provide data type
 
 #####################################################################################
